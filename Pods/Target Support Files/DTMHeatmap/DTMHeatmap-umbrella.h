52c34de466a54ea94896637dfa24877701f387bf
Ondrej Stocek <ondrej.stocek@strv.com>
2016-04-20 21:03:01 +0200

    added DTMHeatmap source files

---
+++ Pods/Target Support Files/DTMHeatmap/DTMHeatmap-umbrella.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+#import "DTMHeatmapRenderer.h"
+#import "DTMDiffHeatmap.h"
+#import "DTMHeatmap.h"
+#import "DTMColorProvider.h"
+#import "DTMDiffColorProvider.h"
+
+FOUNDATION_EXPORT double DTMHeatmapVersionNumber;
+FOUNDATION_EXPORT const unsigned char DTMHeatmapVersionString[];
+
---
+++ Pods/Target Support Files/Pods-OSWifiHeatMap/Pods-OSWifiHeatMap-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_OSWifiHeatMapVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_OSWifiHeatMapVersionString[];
+
