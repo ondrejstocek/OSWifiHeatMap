b4a0d8463af000f56579334d728da2db81f3c6b3
Ondrej Stocek <ondrej.stocek@strv.com>
2016-04-20 19:26:39 +0200

    added map view

--- OSWifiHeatMap/ViewController.swift
+++ OSWifiHeatMap/ViewController.swift
@@ -7,19 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+	@IBOutlet weak var mapView: MKMapView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
+	
+	// MARK: - Map View Delegate
+	
+//	func mapView(mapView: MKMapView, rendererForOverlay overlay: MKOverlay) -> MKOverlayRenderer {
+//	}
 }
-
