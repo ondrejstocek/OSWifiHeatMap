b5a71fe039ba08a8a7473f48910bbcbb615a5951
Ondrej Stocek <ondrej.stocek@strv.com>
2016-04-20 20:58:10 +0200

    added zoom to current location

--- OSWifiHeatMap/AppDelegate.swift
+++ OSWifiHeatMap/AppDelegate.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
+	var locationManager: CLLocationManager?
 
 
 	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-		// Override point for customization after application launch.
+		
+		self.locationManager = CLLocationManager()
+		self.locationManager?.requestWhenInUseAuthorization()
+
 		return true
 	}
 
