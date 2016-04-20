9f2f59c9d1209c8c2f214196a038e55adae6ad1b
Ondrej Stocek <ondrej.stocek@strv.com>
2016-04-20 19:12:45 +0200

    xcode project created

---
+++ OSWifiHeatMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OSWifiHeatMap
+//
+//  Created by Ondrej Stocek on 20/04/16.
+//  Copyright © 2016 Ondrej Stocek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
